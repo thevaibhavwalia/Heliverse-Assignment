4da57833ff0f749b362d87bdbf3b27be44f946db
thevaibhavwalia <129891354+thevaibhavwalia@users.noreply.github.com>
2024-08-12 12:14:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ import { getTeachers } from './controllers/userController.js';
 
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: 'https://heliverse-frontend-blue.vercel.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 204 
+};
+
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions)); //
 app.use(express.json());
 app.use(bodyParser.json());
 dotenv.config();
